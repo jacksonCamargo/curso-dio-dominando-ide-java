04099c9fc7c10e3ea2db3bac5736f95e0fc4b7e4
jackson <jacksonanderson1@hotmail.com>
2022-08-11 00:57:29 -0300

    subindo os arquivos do java

---
+++ src/java_conceito/java_conceito.java
@@ -0,0 +1,31 @@
+package java_conceito;
+
+import java_conceito.model.Gato;
+
+public class java_conceito {
+  
+	public static void main(String[] args) {
+		
+		Gato  gato = new Gato();
+		Livros  livros = new Livros();
+		
+		System.out.println(gato);	
+		
+		
+		
+		
+		/*int a = 32;
+		int b = 32;
+		
+		int soma = a +b;
+		*/
+		//System.out.println("hey there, i'm jackson " + soma);
+		
+	}
+		
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
